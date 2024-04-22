78e5af524f16670743d366e365fd5be5fbb93cf6
helloswx <helloswx@126.com>
2024-04-22 21:29:42 +0800

    make-ready

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Placeholder for any interactive JavaScript
+/*<header class="site-header">
+<img src="ccs.png" alt="ACM SIGSAC Logo" class="ccs-logo"> <!-- ACM SIGSAC Logo -->
+<h1>11th ACM Workshop on Adaptive and Autonomous Cyber Defense (AACD 2024)</h1>
+
+<nav class="site-nav">
+    <ul>
+        In conjunction with the&nbsp;
+        <li><a href="https://www.sigsac.org/ccs/CCS2024/">ACM Conference on Computer and Communications Security (CCS)</a></li>
+        <li>October 14-18, 2024 Salt Lake City, U.S.A.</li>
+        <!-- Other navigation items -->
+    </ul>
+</nav>
+</header>*/
